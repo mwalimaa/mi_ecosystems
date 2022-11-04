1042fa9a1e4617c625c3747ab6aba7cc1a533219
mwalimaa <mwalimaa@gmail.com>
2022-11-03 19:58:47 -0700

    got terra crop/mask to work

--- scratch.R
+++ scratch.R
@@ -3,41 +3,38 @@
 # then I will explore the code using the terra package
 
 library(tidyverse)
+library(terra)
 library(sf)
-library(raster)
-library(terra) # not gonna use terra
 
 
 # import the michigan counties shapefile from disk
 mico <- st_read("C:/Users/mwali/mwalimaa/Projects/SpatialData/Michigan/Counties_v17a/Counties_v17a.shp") %>%
   st_transform(crs = 5070) %>%
-  terra::vect()
-mi <- terra::union(mico)
+  st_union() %>%
+  vect()
+mi <- st_read("C:/Users/mwali/mwalimaa/Projects/SpatialData/us_state_bounds/tl_2022_us_state.shp") %>%
+  st_transform(5070) %>%
+  filter(NAME == "Michigan") %>%
+  vect()
 
 
 # import the most recent landfire data and crop mask to mi
-us_bps <- terra::rast("C:/Users/mwali/mwalimaa/Projects/SpatialData/LANDFIRE/LF2020_BPS_220_CONUS/Tif/LC20_BPS_220.tif")
+us_bps <- rast("C:/Users/mwali/mwalimaa/Projects/SpatialData/LANDFIRE/LF2020_BPS_220_CONUS/Tif/LC20_BPS_220.tif")
+us_bps # notice it has a color table
 
-plot(us_bps)
+# save color table for later
+cols <- coltab(us_bps)
+# remove color table from raster
+coltab(us_bps) <- NULL
+
+# crop and mask to the shapefile
+mi_bps <- crop(us_bps, mi, mask = T)
+# reassign color table to raster
+coltab(mi_bps) <- cols
 
 
-bps_c <- us_bps %>% terra::crop(mi) # not working
+plot(mi_bps) # looks good
 
-plot(bps_c)
-
-
-
-
-
-
-###########################################33
-
-us_evt <- terra::rast("C:/Users/mwali/mwalimaa/Projects/SpatialData/LANDFIRE/LF2020_EVT_220_CONUS/Tif/LC20_EVT_220.tif")
-
-# crop and mask rasters to mi
-mi_bps <- us_bps %>%
-  crop(mi) %>%
-  mask(mi)
-mi_evt <- us_evt %>%
-  crop(mi) %>%
-  mask(mi)
+writeRaster(mi_bps, "C:/Users/mwali/mwalimaa/Projects/SpatialData/LFtest/mi_bps_crop.tif",
+            gdal = c("COMPRESS=NONE", "TFW=YES"),
+            datatype = "INT2S")
