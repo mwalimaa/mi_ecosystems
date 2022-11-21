67876c902067972ac1d7823b6e4f41e326fb38aa
mwalimaa <mwalimaa@gmail.com>
2022-11-20 18:13:22 -0800

    first publication on Git. Removed rasters and large files. They still exist elsewhere

--- scratch.R
+++ scratch.R
@@ -2,9 +2,15 @@
 # first I will see how it updates to github
 # then I will explore the code using the terra package
 
+# most recent version is on devtools
+install.packages('terra', repos='https://rspatial.r-universe.dev')
+
 library(tidyverse)
 library(terra)
 library(sf)
+
+################################
+## Reading, cropping, masking, writing the raster
 
 
 # import the michigan counties shapefile from disk
@@ -20,21 +26,62 @@ mi <- st_read("C:/Users/mwali/mwalimaa/Projects/SpatialData/us_state_bounds/tl_2
 
 # import the most recent landfire data and crop mask to mi
 us_bps <- rast("C:/Users/mwali/mwalimaa/Projects/SpatialData/LANDFIRE/LF2020_BPS_220_CONUS/Tif/LC20_BPS_220.tif")
-us_bps # notice it has a color table
+us_evt <- rast("C:/Users/mwali/mwalimaa/Projects/SpatialData/LANDFIRE/LF2020_EVT_220_CONUS/Tif/LC20_EVT_220.tif")
 
-# save color table for later
+### save color table for later
 cols <- coltab(us_bps)
-# remove color table from raster
-coltab(us_bps) <- NULL
+saveRDS(cols, file = "./bps_coltab.RData")
+saveRDS(coltab(us_evt), file = "./evt_coltab.RData")
+### remove color table from raster
+### coltab(us_bps) <- NULL
 
-# crop and mask to the shapefile
+
+# crop and mask raster to the shapefile
 mi_bps <- crop(us_bps, mi, mask = T)
-# reassign color table to raster
-coltab(mi_bps) <- cols
-
+### reassign color table to raster
+### coltab(mi_bps) <- cols
 
 plot(mi_bps) # looks good
 
-writeRaster(mi_bps, "C:/Users/mwali/mwalimaa/Projects/SpatialData/LFtest/mi_bps_crop.tif",
+# confirm INT2S datatype
+str(mi_bps)
+
+# write the raster
+writeRaster(mi_bps, "./outputs/mi_bps_crop.tif",
             gdal = c("COMPRESS=NONE", "TFW=YES"),
-            datatype = "INT2S")
+            datatype = "INT2S",
+            overwrite = T)
+
+# remove unneccessary large raster
+rm(us_bps)
+
+## Randy, were you able to export the colortable with the GeoTIF?
+
+###################################
+## extracting information
+
+activeCat(mi_bps) <- "Value"
+plot(mi_bps)
+
+x <- values(mi_bps, dataframe = T, na.rm = T) %>%
+  table(dnn = "Value") %>%
+  as.data.frame() %>%
+  mutate_all(as.character) %>%
+  mutate_all(as.integer) %>%
+  left_join(cats(mi_bps)[[1]], by = "Value")
+
+x2 <- x %>% filter(Freq != 0)
+
+install.packages("foreign")
+
+# clean memory
+gc()
+# foreign package write as dbf file, same name as .tif but .tif.vat.dbf
+
+foreign::write.dbf(x2, "./outputs/mi_bps_crop.tif.vat.dbf")
+
+
+##########################################################
+### write bounding box and geojson and upload to LF
+
+  st_write("./outputs/aoi_bbox.geojson", delete_dsn = T)
